40c3e6beb3534efb2be7ec73acb364979064312f
Raymond Nie <nieraymond@gmail.com>
2017-01-10 01:29:14 -0500

    resume hover

---
+++ js/resumehover.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $(".happy").hover(
+	function(){$(".selfie").attr("src","../assets/hopeful.jpg");},
+	function(){ $(".selfie").attr("src","../assets/selfie.jpg");});
+});
