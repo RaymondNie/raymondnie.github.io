c9dbe79fe8accd6b68c55bc9648d201ecea93f0f
Raymond Nie <nieraymond@gmail.com>
2017-12-22 23:47:41 -0500

    Rehaul of website, removed all styling and old projects

--- js/resumehover.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $(".happy").hover(
-	function(){$(".selfie").attr("src","../assets/hopeful.jpg");},
-	function(){ $(".selfie").attr("src","../assets/selfie.jpg");});
-});
-
